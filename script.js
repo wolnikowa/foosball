4d25dbe08765560f2f5fe37bc3ac432a8a479aa5
Alicja <53865337+alicjakempa@users.noreply.github.com>
2021-07-07 10:42:09 +0200

    creating project with webpack

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.use(express.static('dist'));
+
+app.listen(4000);
+
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '/',
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ""
+                        }
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    },
+
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|otf)$/,
+                loader: 'file-loader',
+            }
+
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ]
+};
