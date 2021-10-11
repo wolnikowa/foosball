18c9a49e4af6cadd0048544a91c7b92996457295
Alicja <53865337+alicjakempa@users.noreply.github.com>
2021-10-11 17:00:44 +0200

    Added font and repaired router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Nav from "./components/Nav";
 import DefaultPage from "./components/Main";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { IntlProvider } from "react-intl";
-
 import Boxscore from "./components/BoxScore";
 import ActiveTournamentBody from "./components/ActiveTournamentsBody";
-
-
 import "./globalStyles.scss";
 import GroupsTable from "./components/ActiveTournamentGroups";
 import TournamentEdition from "./components/TournamentEdition";
 import TeamList from "./components/Teams";
+import ActiveTournaments from "./components/ActiveTournaments";
+import Tournament from "./components/Tournament";
+import TournamentLadder from "./components/TournamentLadder";
+import GroupResults from "./components/GroupResults";
 
 const App = () => (
 	<IntlProvider locale="pl" defaultLocale="pl">
@@ -24,31 +25,32 @@ const App = () => (
 					<Switch>
 						<Route exact path="/">
 							<DefaultPage />
-
-
-							<Boxscore/>
-
-							
+							<Boxscore />
+						</Route>
+						<Route exact path="/Active-tournament">
+							<ActiveTournamentBody />
+							<TournamentEdition />
+							<TeamList />
+						</Route>
+						<Route exact path="/Active-tournament/tournament-groups">
+							<Tournament />
 							<GroupsTable />
-						
+						</Route>
+						<Route exact path="/Active-tournament/tournament-group-results">
+							<Tournament />
+							<GroupResults />
+						</Route>
+						<Route exact path="/Active-tournament/tournament-table">
+							<Tournament />
+							<TournamentLadder />
 						</Route>
 					</Switch>
 				</div>
 			</div>
-
-
-			<Switch>
-						<Route exact path="/Active-tournament">
-							
-							<ActiveTournamentBody />
-							<TournamentEdition />
-							<TeamList />
-  
-						</Route>
-					</Switch>
 
 		</Router>
 	</IntlProvider>
 );
 
 export default App;
+
